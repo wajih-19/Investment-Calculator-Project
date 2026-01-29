7d2563ba4abac15fe487d3dbe6814aabed0b19b7
Syed Wajih <syedwajih@gmail.com>
2026-01-29 22:54:43 +0500

    done

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import logo from "../assets/investment-calculator-logo.png";
+import classes from './Header.module.css';
+
+export default function Header(){
+    return(
+      <header className={classes.header}>
+        <img src={logo} alt="logo" />
+        <h1>Investment Calculator</h1>
+      </header>
+    );
+}
